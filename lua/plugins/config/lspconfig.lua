375c144a5de2e8e69b66f3541bd5865d2047b467
nachohotz <juanhotz611@gmail.com>
2022-09-16 16:44:26 -0300

    FEAT: coc like mappings

--- lua/plugins/config/lspconfig.lua
+++ lua/plugins/config/lspconfig.lua
@@ -11,3 +11,10 @@ require('lspconfig').jsonls.setup{}
 require('lspconfig').prismals.setup{}
 require('lspconfig').rust_analyzer.setup{}
 require('lspconfig').tailwindcss.setup{}
+
+-- Show hover
+vim.keymap.set('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>')
+-- Jump to definition
+vim.keymap.set('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>')
+--Open code actions using the default lsp UI, if you want to change this please see the plugins above
+vim.keymap.set('n', '<leader>cda', '<Cmd>lua vim.lsp.buf.code_action()<CR>')
