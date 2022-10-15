fef2f20903cc626ac93d8565f38e324f72b6cb13
nachohotz <juanhotz611@gmail.com>
2022-10-15 12:13:15 -0300

    feat(plugins): toggleterm mappings. Cmp window comment

---
+++ lua/plugins/config/toggleterm_config.lua
@@ -0,0 +1,12 @@
+local opts = { buffer = 0 }
+
+function _G.set_terminal_keymaps()
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'ii', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<Leader>h', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<Leader>j', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<Leader>k', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<Leader>l', [[<Cmd>wincmd l<CR>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
