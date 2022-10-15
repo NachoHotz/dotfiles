c5feebe02169cfde5c45dd0f728cfee111e4bd81
nachohotz <juanhotz611@gmail.com>
2022-10-15 15:55:10 -0300

    feat(plugins): lualine clock

--- lua/plugins/config/lualine.lua
+++ lua/plugins/config/lualine.lua
@@ -1,5 +1,17 @@
 require('lualine').setup({
   options = {
-    theme = 'auto'
+    theme = 'auto',
+  },
+  sections = {
+    lualine_z = { 'location', 'os.date("%H:%M", os.time())' }
   }
 })
+
+-- Trigger rerender of status line every second for clock
+if _G.Statusline_timer == nil then
+  _G.Statusline_timer = vim.loop.new_timer()
+else
+  _G.Statusline_timer:stop()
+end
+_G.Statusline_timer:start(0, 1000, vim.schedule_wrap(
+  function() vim.api.nvim_command('redrawstatus') end))
