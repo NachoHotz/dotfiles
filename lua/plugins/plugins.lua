63d14220495f4e2f5e3b47aa91c16c79cbdbf2d7
nachohotz <juanhotz611@gmail.com>
2022-10-17 16:58:03 -0300

    task(plugins): change vim plug for packer. Remove autoload folder

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,81 +1,77 @@
 --------------------------------Plugins--------------------------------------------
-local defaults = require "utils.get_defaults"
-local pluginsFolder = defaults.get_plugged_folder_path()
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
 
-vim.call('plug#begin', pluginsFolder)
+  -- colorschemes
+  use 'marko-cerovac/material.nvim'
 
-local Plug = vim.fn['plug#']
+  -- code action lightbulb
+  use 'kosayoda/nvim-lightbulb'
 
--- colorschemes
-Plug 'marko-cerovac/material.nvim'
+  -- status && bufferline
+  use 'nvim-lualine/lualine.nvim'
+  use 'akinsho/bufferline.nvim'
+  use 'kyazdani42/nvim-web-devicons'
 
--- code action lightbulb
-Plug 'kosayoda/nvim-lightbulb'
+  -- nvim tree
+  use 'kyazdani42/nvim-tree.lua'
 
--- status && bufferline
-Plug 'nvim-lualine/lualine.nvim'
-Plug 'akinsho/bufferline.nvim'
-Plug 'kyazdani42/nvim-web-devicons'
+  -- terminal
+  use 'akinsho/nvim-toggleterm.lua'
 
--- nvim tree
-Plug 'kyazdani42/nvim-tree.lua'
+  -- syntax
+  use 'lukas-reineke/indent-blankline.nvim' -- intentation
+  use 'ntpeters/vim-better-whitespace' -- see whitespaces in red
+  use 'tpope/vim-surround' -- commands to surround text with quotes, parentheses, etc
+  use 'windwp/nvim-autopairs' -- auto close parentheses and such
+  use 'windwp/nvim-ts-autotag' -- auto close html tags in tsx files
+  use 'b0o/schemastore.nvim' -- json config schemas
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'norcalli/nvim-colorizer.lua' -- css color properties highlight
+  use 'folke/lsp-colors.nvim' -- Add some LSP highlight groups if missing from a colorscheme
 
--- terminal
-Plug 'akinsho/nvim-toggleterm.lua'
+  -- html live server
+  use 'turbio/bracey.vim'
 
--- syntax
-Plug 'lukas-reineke/indent-blankline.nvim' -- intentation
-Plug 'ntpeters/vim-better-whitespace' -- see whitespaces in red
-Plug 'tpope/vim-surround' -- commands to surround text with quotes, parentheses, etc
-Plug 'windwp/nvim-autopairs' -- auto close parentheses and such
-Plug 'windwp/nvim-ts-autotag' -- auto close html tags in tsx files
-Plug 'b0o/schemastore.nvim' -- json config schemas
-Plug 'nvim-treesitter/nvim-treesitter'
-Plug 'norcalli/nvim-colorizer.lua' -- css color properties highlight
-Plug 'folke/lsp-colors.nvim' -- Add some LSP highlight groups if missing from a colorscheme
+  --- LSP && Mason
+  use 'onsails/lspkind.nvim'
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'stevearc/dressing.nvim' -- nvim input ui
 
--- html live server
-Plug 'turbio/bracey.vim'
+  -- Telescope
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
 
---- LSP && Mason
-Plug 'onsails/lspkind.nvim'
-Plug 'neovim/nvim-lspconfig'
-Plug 'williamboman/mason.nvim'
-Plug 'williamboman/mason-lspconfig.nvim'
-Plug 'stevearc/dressing.nvim' -- nvim input ui
+  -- Git integration
+  use 'tpope/vim-fugitive' -- git commands
+  use 'lewis6991/gitsigns.nvim' -- git change signs in buffers
 
--- Telescope
-Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-telescope/telescope.nvim'
+  -- copilot
+  use 'zbirenbaum/copilot.lua'
+  use 'github/copilot.vim'
+  use 'zbirenbaum/copilot-cmp'
 
--- Git integration
-Plug 'tpope/vim-fugitive' -- git commands
-Plug 'lewis6991/gitsigns.nvim' -- git change signs in buffers
+  -- cmp
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'ray-x/lsp_signature.nvim' -- show function signature in popup window when typing
 
--- copilot
-Plug 'zbirenbaum/copilot.lua'
-Plug 'github/copilot.vim'
-Plug 'zbirenbaum/copilot-cmp'
+  -- snippets
+  use 'SirVer/ultisnips'
+  use 'quangnguyen30192/cmp-nvim-ultisnips'
+  use 'honza/vim-snippets'
+  use 'mlaursen/vim-react-snippets'
 
--- cmp
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'hrsh7th/cmp-nvim-lua'
-Plug 'hrsh7th/cmp-buffer'
-Plug 'hrsh7th/cmp-path'
-Plug 'hrsh7th/cmp-cmdline'
-Plug 'hrsh7th/nvim-cmp'
-Plug 'ray-x/lsp_signature.nvim' -- show function signature in popup window when typing
+  use 'numToStr/Comment.nvim'
 
--- snippets
-Plug 'SirVer/ultisnips'
-Plug 'quangnguyen30192/cmp-nvim-ultisnips'
-Plug 'honza/vim-snippets'
-Plug 'mlaursen/vim-react-snippets'
+  use 'gelguy/wilder.nvim' -- wild menu
 
-Plug 'numToStr/Comment.nvim'
+  use 'j-hui/fidget.nvim' -- show LSP clients progress
 
-Plug('gelguy/wilder.nvim', { ['do'] = vim.fn['UpdateRemotePlugins'] }) -- wild menu
-
-Plug 'j-hui/fidget.nvim' -- show LSP clients progress
-
-vim.call('plug#end')
+end)
