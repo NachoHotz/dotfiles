5724bfe7013b85190f837d0efafe4b4e9d4fe879
nachohotz <juanhotz611@gmail.com>
2022-10-17 10:36:26 -0300

    task(general): more settings

--- lua/general/settings.lua
+++ lua/general/settings.lua
@@ -1,22 +1,31 @@
 ---------------------------------vim config----------------------------
 local set = vim.opt
 
-set.shiftwidth = 2 -- Indent with 2 spaces
-set.incsearch = true -- Incremental search (find as you type)
-set.expandtab = true -- Use spaces over tabs
-set.swapfile = false -- No swap file
-set.mouse = '' -- ignore mouse
-set.number = true -- Show line numbers
-set.smartindent = true -- Smart auto-indenting
+local current_os = vim.loop.os_uname().sysname
+
+local linux_shell = 'zsh'
+local windows_shell = 'powershell'
+
+local default_shell = current_os == 'Linux' and linux_shell or windows_shell
+
+set.shiftwidth     = 2 -- Indent with 2 spaces
+set.incsearch      = true -- Incremental search (find as you type)
+set.expandtab      = true -- Use spaces over tabs
+set.swapfile       = false -- No swap file
+set.mouse          = '' -- ignore mouse
+set.number         = true -- Show line numbers
+set.smartindent    = true -- Smart auto-indenting
+set.ai             = true
 set.relativenumber = true -- Relative line numbers
-set.scrolloff = 8 -- Lines of context
-set.hlsearch = false -- Don't highlight matches on previous search
-set.hidden = true -- Enable modified buffers in background
-set.showmode = false -- We don't need to see things like -- INSERT -- anymore
-set.wrap = true -- Enable line wrap
-set.cmdheight = 1 -- Make the command-line shorter
-set.autochdir = true -- Change working directory with :lcd
-set.termguicolors = true -- True color support
-set.clipboard = 'unnamedplus'
+set.scrolloff      = 8 -- Lines of context
+set.hlsearch       = false -- Don't highlight matches on previous search
+set.hidden         = true -- Enable modified buffers in background
+set.showmode       = false -- We don't need to see things like -- INSERT -- anymore
+set.wrap           = true -- Enable line wrap
+set.cmdheight      = 1 -- Make the command-line shorter
+set.autochdir      = true -- Change working directory with :lcd
+set.termguicolors  = true -- True color support
+set.shell          = default_shell
+set.clipboard      = 'unnamedplus'
 
 vim.g.mapleader = ' '
