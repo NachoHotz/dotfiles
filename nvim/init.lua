4e4abfa40cd82f378339822a93f02c08c97f47c3
nachohotz <juanhotz611@gmail.com>
2022-10-20 15:52:12 -0300

    task(general): rename plugins.lua and general folder

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 ------------------------------Sources-------------------------------
-require('general.settings')
-require('general.mappings')
-require('plugins.plugins')
+require('core.settings')
+require('core.mappings')
+require('plugins.plugins-setup')
 require('plugins.config')
