cb1f1c01dfb490291b2feafb8ca2dd26b092d890
nachohotz <juanhotz611@gmail.com>
2022-10-27 15:22:43 -0300

    task(plugins-config): material config

--- nvim/lua/plugins/setup/material.lua
+++ nvim/lua/plugins/setup/material.lua
@@ -1,12 +1,52 @@
 vim.g.material_style = 'deep ocean'
 
 require('material').setup {
-  plugins = {
-    "gitsigns",
-    "nvim-cmp",
-    "nvim-tree",
-    "telescope",
-    "indent-blankline",
+  styles = {
+    comments = { italic = true },
+    strings = { bold = false },
+    keywords = { underline = false },
+    functions = { bold = false, undercurl = false },
+    variables = {},
+    operators = {},
+    types = {},
   },
-  lualine_style = 'default'
+
+  plugins = {
+    -- "dap",
+    -- "dashboard",
+    "gitsigns",
+    -- "hop",
+    "indent-blankline",
+    -- "lspsaga",
+    -- "mini",
+    -- "neogit",
+    "nvim-cmp",
+    -- "nvim-navic",
+    "nvim-tree",
+    -- "sneak",
+    "telescope",
+    -- "trouble",
+    -- "which-key",
+  },
+
+  disable = {
+    colored_cursor = false, -- Disable the colored cursor
+    borders = false, -- Disable borders between verticaly split windows
+    background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
+    term_colors = false, -- Prevent the theme from setting terminal colors
+    eob_lines = false -- Hide the end-of-buffer lines
+  },
+
+  high_visibility = {
+    lighter = false, -- Enable higher contrast text for lighter style
+    darker = false -- Enable higher contrast text for darker style
+  },
+
+  lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+
+  async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+
+  custom_colors = nil, -- If you want to everride the default colors, set this to a function
+
+  custom_highlights = {}, -- Overwrite highlights with your own
 }
