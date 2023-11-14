2104d5a79ab35e35191eb750074d89334ff45518
Juan Hotz <juan.hotz@comafi.com.ar>
2023-11-14 11:57:21 -0300

    feat: new nvim config

--- nvim/lua/plugins/setup/bufferline.lua
+++ nvim/lua/plugins/setup/bufferline.lua
@@ -1,7 +1,26 @@
 ---@diagnostic disable: unused-local
-require('bufferline').setup {
+local bufferline = require('bufferline')
+
+bufferline.setup {
   options = {
+    mode = 'buffers',
+    style_preset = bufferline.style_preset.default,
+    themable = true,
+    numbers = 'none',
+    close_command = "bdelete! %d",
+    right_mouse_command = "bdelete! %d",
+    left_mouse_command = "buffer %d",
+    middle_mouse_command = nil,
+    close_icon = "",
+    buffer_close_icon = "",
+    modified_icon = "",
+    left_trunc_marker = "",
+    right_trunc_marker = "",
     diagnostics = "nvim_lsp",
+    indicator = {
+      icon = "",
+      style = 'underline',
+    },
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
       local icon = level:match("error") and " " or " "
       return " " .. icon .. count
